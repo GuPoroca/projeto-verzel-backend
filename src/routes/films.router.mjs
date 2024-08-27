b00f27f6feedbfbab8f553f59a9d4e3e843b5568
Poroca <gustavoporoca@gmail.com>
2024-08-27 11:16:00 -0300

    route naming

--- src/routes/films.router.mjs
+++ src/routes/films.router.mjs
@@ -6,12 +6,12 @@ const routes = Router();
 const filmsController = new FilmsController();
   
   //searchFilms
-  routes.get('/api/films', (request, response) =>
+  routes.post('/api/films', (request, response) =>
     filmsController.search(request, response)
   );
 
   //favorite
-  routes.post('/api/films', (request, response) =>
+  routes.post('/api/favorite', (request, response) =>
     filmsController.favorite(request, response)
   );
 
